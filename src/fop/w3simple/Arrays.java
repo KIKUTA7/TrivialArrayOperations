bad0b91bed3fd44d06ad3e176a1476979c8a51b0
Helmut Seidl <helmut.seidl@tum.de>
2020-10-14 13:11:32 +0200

    Exercise-Template pushed by Artemis

---
+++ src/fop/w3simple/Arrays.java
@@ -0,0 +1,23 @@
+package fop.w3simple;
+
+public class Arrays extends MiniJava {
+    public static void print(int[] arr) {
+	// TODO
+    }
+
+    public static void minAndMax(int[] arr) {
+	// TODO
+    }
+
+    public static int[] invert(int[] arr) {
+	// TODO
+    }
+
+    public static int[] cut(int[] arr, int len) {
+	// TODO
+    }
+
+    public static int[] linearize(int[][] arr) {
+	// TODO
+    }
+}
